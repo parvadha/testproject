d5f92a39e00c65ee2f6585e4d5946e8528d6f97a
parvadha <parvadha.ramar@gmail.com>
2024-08-06 12:31:12 +0530

    map-filter--high emp salary list

--- demo.js
+++ demo.js
@@ -1,7 +1,24 @@
-const arr=[1,2,3,4,5]
-console.log(arr)
+// Exercise #2->To display employee name and salary using map()
+// -----------
+const emp=[
+{eid:1,ename:"Prabhu",salary:40000,city:'Chennai'},
+{eid:2,ename:"Bala",salary:50000,city:'Madurai'},
+{eid:3,ename:"Deva",salary:20000,city:'Madurai'},
+{eid:4,ename:"Naveen",salary:40000,city:'Trichy'},
+{eid:5,ename:"Gowtham",salary:10000,city:'Chennai'},
+{eid:6,ename:"Selva",salary:60000,city:'Chennai'},
+];
+//newArr=oldArr.map(func);
+// const newEmp=emp.map(e=>e.city);
+// console.log(newEmp);
 
-let table=`<table border="1"><tr><th>Number</th><th>Square</th></tr>`
-arr.map(a=>table+=`<tr><td>${a}</td><td>${a*a}</td></tr>`)
-let div=document.querySelector(".container")
-div.innerHTML=table
+//filter(func)
+const highEmpSal=emp.filter(e=>e.salary>=50000)
+
+
+let table="<table border='1px'><tr><th>EmpName</th><th>Salary</th></tr>";
+highEmpSal.map((e)=>{
+    table+=`<tr><td>${e.ename}</td><td>${e.salary}</td></tr>`;
+});
+table+='</table>';
+document.querySelector('.container').innerHTML=table;

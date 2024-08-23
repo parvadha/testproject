cdf52aa2f8c432c3a2f132be8d2164c80dbaae4c
parvadha <parvadha.ramar@gmail.com>
2024-08-23 12:48:21 +0530

    fetch demo added

--- demo.js
+++ demo.js
@@ -1,24 +1,29 @@
-// Exercise #2->To display employee name and salary using map()
-// -----------
-const emp=[
-{eid:1,ename:"Prabhu",salary:40000,city:'Chennai'},
-{eid:2,ename:"Bala",salary:50000,city:'Madurai'},
-{eid:3,ename:"Deva",salary:20000,city:'Madurai'},
-{eid:4,ename:"Naveen",salary:40000,city:'Trichy'},
-{eid:5,ename:"Gowtham",salary:10000,city:'Chennai'},
-{eid:6,ename:"Selva",salary:60000,city:'Chennai'},
-];
-//newArr=oldArr.map(func);
-// const newEmp=emp.map(e=>e.city);
-// console.log(newEmp);
+let usersData=[]
+const containerDiv=document.querySelector(".container")
+const getUsersBtn=document.querySelector("#users")
 
-//filter(func)
-const highEmpSal=emp.filter(e=>e.salary>=50000)
+// function getData()
+// {
 
+// fetch("https://jsonplaceholder.typicode.com/users").then(data=>data.json()).then(data=>usersData=data).then(()=>displayData())
+            
+// }
+async function getData()
+{
+let data=await fetch("https://jsonplaceholder.typicode.com/users")
+let response=await data.json()
+usersData=await response
+displayData()
+            
+}
+function displayData()
+{
+    
+    let table="<table><tr><th>Name</th><th>emailId</th></tr>"
+    usersData && usersData.map(user=>table+=`<tr><td>${user.name}</td><td>${user.email}</td></tr>`)
+     
+     containerDiv.innerHTML=table
+   
+}
 
-let table="<table border='1px'><tr><th>EmpName</th><th>Salary</th></tr>";
-highEmpSal.map((e)=>{
-    table+=`<tr><td>${e.ename}</td><td>${e.salary}</td></tr>`;
-});
-table+='</table>';
-document.querySelector('.container').innerHTML=table;
+getUsersBtn.addEventListener('click',getData)
